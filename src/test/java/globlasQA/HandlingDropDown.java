7d1dcc23991ba6eb26335261d341851f9d824712
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-03-18 21:34:31 +0530

    First Execution

--- src/test/java/globlasQA/HandlingDropDown.java
+++ src/test/java/globlasQA/HandlingDropDown.java
@@ -2,9 +2,14 @@ package globlasQA;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class HandlingDropDown {
 	
@@ -15,7 +20,16 @@ public class HandlingDropDown {
 		driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.globalsqa.com/demo-site/select-dropdown-menu/");
 //		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void countryDropDown() {
+		WebElement dropDown = driver.findElement(By.xpath("//div[@class='single_tab_div resp-tab-content resp-tab-content-active']//p//select"));
+		Select dp = new Select(dropDown);
+		dp.selectByVisibleText("India");
+		
 	}
 	
 	@AfterClass
