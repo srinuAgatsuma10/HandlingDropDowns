a726ec2bb1453541578e377bb8d5d990407cc6c4
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-03-22 18:15:54 +0530

    OrangeHRM Setup

--- src/test/java/orangeHRM/HandlingHidden_DropDown.java
+++ src/test/java/orangeHRM/HandlingHidden_DropDown.java
@@ -1,7 +1,38 @@
 package orangeHRM;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
 public class HandlingHidden_DropDown {
 	
-	// Need to Implement
+
+	WebDriver driver;
+
+	@BeforeClass()
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize();
+	}
+	
+	
+	
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	public void logIN(WebDriver driver) {
+		driver.findElement(By.xpath("")).sendKeys("");
+		driver.findElement(By.xpath("")).sendKeys("");
+		driver.findElement(By.xpath("")).sendKeys("");
+	}
 	
 }
