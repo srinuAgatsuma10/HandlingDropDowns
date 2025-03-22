946f04a08c077593fa2bbb93fc6cb7e566bd0fc6
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-03-22 19:25:07 +0530

    Google Chrome Final Commit

--- src/test/java/GoogleChrome/Handling_SuggestedDropDown.java
+++ src/test/java/GoogleChrome/Handling_SuggestedDropDown.java
@@ -1,10 +1,15 @@
 package GoogleChrome;
 
 import java.time.Duration;
+import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class Handling_SuggestedDropDown {
 
@@ -18,15 +23,31 @@ public class Handling_SuggestedDropDown {
 		driver.get("https://www.google.com/");
 		driver.manage().window().maximize();
 	}
-	
+
 	@Test
 	public void suggestedDropDown() {
-		
+
+		// Text Area/Search Button
+		driver.findElement(By.xpath("//textarea[@id='APjFqb']")).sendKeys("Whey protein");
+		// Accessing suggested elements
+		List<WebElement> autoOptoin = driver
+				.findElements(By.xpath("//ul[@class=\"G43f7e\"]//li//div[@role=\"option\"]"));
+		System.out.println(autoOptoin.size());
+		// Get suggested text and select given search option
+		for (int i = 0; i < autoOptoin.size(); i++) {
+			System.out.println("Suggested Text: " + autoOptoin.get(i).getText());
+		}
+		for (int i = 0; i < autoOptoin.size(); i++) {
+			if (autoOptoin.get(i).getText().equals("whey protein powder benefits")) {
+				autoOptoin.get(i).click();
+				break;
+			}
+		}
 	}
-	
+
 	@AfterClass
 	public void tearDown() {
 		driver.quit();
 	}
-	
+
 }
