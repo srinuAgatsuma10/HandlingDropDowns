f27f71853bd761904666ad2b9b61794fe0208cb1
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-03-22 19:03:01 +0530

    Google Chrome SetUp

--- src/test/java/GoogleChrome/Handling_SuggestedDropDown.java
+++ src/test/java/GoogleChrome/Handling_SuggestedDropDown.java
@@ -1,5 +1,32 @@
 package GoogleChrome;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
 public class Handling_SuggestedDropDown {
 
+	WebDriver driver;
+
+	@BeforeClass()
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void suggestedDropDown() {
+		
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+	
 }
